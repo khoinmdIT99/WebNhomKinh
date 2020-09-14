f909e2214160d25d2f1460760d7f578b9b66ae4f
war-man <40898660+war-man@users.noreply.github.com>
2020-09-15 00:38:04 +0700

    Add project files.

---
+++ WebNhomKinh/Models/QuanLyTinTuc.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using PagedList;
+
+namespace WebNhomKinh.Models
+{
+    public class QuanLyTinTuc
+    {
+        public  IPagedList<TinTuc> DanhSachTinTuc { get; set; }
+        public TinTuc TinTuc { get; set; }
+       
+    }
+}
